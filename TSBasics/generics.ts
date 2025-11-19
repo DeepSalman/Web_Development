90f0812798f5fe140e374bcb46ba2c2677d52c47
Salman <kans6643@gmail.com>
2025-11-19 16:31:52 +0000

    TS basics

--- TSBasics/generics.ts
+++ TSBasics/generics.ts
@@ -4,8 +4,8 @@ function getFirstElement(array: number[]){
     return array[0]
 }
 
-const numbers = [1,2,3]
-const FirstElement=getFirstElement(numbers)
+const numberss = [1,2,3]
+const FirstElement=getFirstElement(numberss)
 
 
 //Type 02
