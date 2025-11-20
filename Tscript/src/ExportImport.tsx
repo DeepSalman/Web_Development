d4069ffab3d2fbe449fa70529edd7eb669531e03
Salman <kans6643@gmail.com>
2025-11-20 15:48:09 +0000

    react creation

--- Tscript/src/ExportImport.tsx
+++ Tscript/src/ExportImport.tsx
@@ -1,2 +1,15 @@
+type CardProps = {
+  title: string;
+  views?: number; // optional
+};
+
+function Card({ title, views }: CardProps) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{views}</p>
+    </div>
+  );
+}
 
 
