a893f249c5a1fec2d3734f4b27fff9b4f42ef08f
Salman <kans6643@gmail.com>
2025-11-20 04:11:11 +0000

    changing everything

---
+++ TSBasics/src/test.ts
@@ -0,0 +1,11 @@
+// console.log("Example 1: ");
+
+// type Greet = (name: string) => string;
+
+// // OLD (Wrong): const greet: Greet = (n) => 'Hello,$(n)';
+// // NEW (Correct):
+// const greet: Greet = (n) => `Hello, ${n}`; 
+
+// console.log(greet("Salman!"));
+
+console.log("Hello");
