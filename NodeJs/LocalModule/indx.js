40e0095edc0b783edb67e45c774871aa70ff74b1
Salman <kans6643@gmail.com>
2025-11-21 12:14:06 +0000

    Local Module

---
+++ NodeJs/LocalModule/indx.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+
+//Importing the function
+//require is import key
+const s1= require('./student')
+
+console.log(s1.name());
+console.log(s1.age());
+console.log(s1.cgpa);
+
+
---
+++ NodeJs/LocalModule/student.js
@@ -0,0 +1,17 @@
+//
+//Created 2 arrow function
+const getName=()=>{
+    return  "Salman ";
+}
+
+const getAge=()=>{
+    return "20";
+}
+
+const cgpa=3.5;
+
+
+//Exporting the function
+exports.name=getName;
+exports.age=getAge;
+exports.cgpa;
