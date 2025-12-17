808963605c3ca0dcfe27d58261be0bef81c4d8ca
Salman <kans6643@gmail.com>
2025-12-17 06:20:56 +0000

    creating end points

--- Fapi/myenv/main.py
+++ Fapi/myenv/main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+import json
+
+def load_data():
+    with open("patients.json","r") as f:
+        data = json.load(f)
+
+        return data
+
 
 app = FastAPI()
 @app.get("/")
@@ -7,5 +15,10 @@ async def hello():
 @app.get("/about")
 async def about():
     return {"message": "A full functional API to manage your patient records !"}
+
+@app.get("/view")
+async def view():
+    data = load_data()
+    return data
 
 
