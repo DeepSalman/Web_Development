d7b9085d241cf3f6fe5c89d8d6b8ccd4843a82ec
Salman <rayhanartz@gmail.com>
2026-05-02 20:34:56 +0600

    Types in Typescript

--- Own/TypescriptCode/build/js/main.js
+++ Own/TypescriptCode/build/js/main.js
@@ -3,6 +3,19 @@ console.log(username);
 let a = 12;
 let b = '6';
 let c = 2;
+let album; //This is a any type
+let salman; //This is a Union type
+let re = /\w+/g; //This is a regular a regular expression type 
+album = 19;
+album = "Nineteen";
+album = true;
+salman = 'hello';
+salman = 19;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isactive;
 console.log(a / c);
 export {};
 //# sourceMappingURL=main.js.map
--- Own/TypescriptCode/src/main.ts
+++ Own/TypescriptCode/src/main.ts
@@ -2,8 +2,30 @@ let username = 'dave'
 
 console.log(username)
 
-let a =12;
-let b = '6';
-let c = 2;
+let a:number =12;
+let b:string = '6';
+let c:number = 2;
+let album:any; //This is a any type
+let salman:string | number; //This is a Union type
+let re:RegExp= /\w+/g; //This is a regular a regular expression type 
+
+album=19;
+album="Nineteen";
+album=true;
+
+salman='hello';
+salman=19;
+
+const sum = (a:number,b:string)=> {
+    return a+b;
+}
+
+
+let postId:string | number;
+let isactive:number | boolean | string ;
+
+
+
 
 console.log(a/c);
+
