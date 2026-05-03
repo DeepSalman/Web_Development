40fc9ebd4299bd38ff343801991d18858c6593d8
Salman <rayhanartz@gmail.com>
2026-05-03 22:43:38 +0600

    Arrays part 1

---
+++ Own/TypescriptCode/src/array.ts
@@ -0,0 +1,44 @@
+let stringArray = ['one','hey','Dave'];
+let guiters = ['strat','les paul',5150];
+let mixedData = ['EVH',1984,'true'];
+
+stringArray[0]='john';
+// stringArray[1]=21; This is going to be a problem ;
+// stringArray.push(32);
+stringArray[0]='32';
+
+guiters[0]=32; //There will be no flags because it is an union type which only accepts strings and numbers
+guiters.unshift('jim'); // unshift is used to insert an element in the very beginning 
+// guiters.unshift(true) we can not assign true because in guitters there is only 2 types 
+
+// stringArray = guiters because stringarray is only string type 
+guiters = stringArray // This will run because guiters can take both types
+mixedData = guiters // this will also work
+
+let test = [] // this will be assigned as any type array ;
+let bands: string[] = [];
+bands.push('hello');
+
+
+//------------Tuples-----------------\\
+
+let mytuple:[string,number,boolean] = ['Dave',42,true]
+mytuple[0]='john';
+mytuple[1]=20;
+mytuple[2]=false;
+
+
+//------------Object-----------------\\
+
+let myobj: object
+myobj = []
+console.log(typeof myobj)
+
+const exampleObj = {
+    prop1:'Dave',
+    prop2:true,
+}
+
+exampleObj.prop1='salman';
+exampleObj.prop2=false;
+
