2eb72a5aa350b1722e294b4a4d98b2caeafed311
Salman <rayhanartz@gmail.com>
2026-05-07 14:08:19 +0600

    payoo app event listener

---
+++ Own/JSAll/payooapp/htmlfiles/scripts.js
@@ -0,0 +1,7 @@
+//loginButton Functionality 
+document.getElementById("login_button").addEventListener("click",function(e){
+    e.preventDefault() //To prevent Loading 
+    console.log("Button clicked") 
+    console.log(e)
+
+})
