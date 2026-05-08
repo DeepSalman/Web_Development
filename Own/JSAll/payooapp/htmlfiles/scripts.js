8853d8c2acc30645c82dbd4ea8312fc2f36a0a6f
Salman <rayhanartz@gmail.com>
2026-05-08 14:27:06 +0600

    Login page action Button creation

--- Own/JSAll/payooapp/htmlfiles/scripts.js
+++ Own/JSAll/payooapp/htmlfiles/scripts.js
@@ -1,7 +1,20 @@
 //loginButton Functionality 
 document.getElementById("login_button").addEventListener("click",function(e){
-    e.preventDefault() //To prevent Loading 
-    console.log("Button clicked") 
-    console.log(e)
+    e.preventDefault()
+    const mobileNumber = 12345
+    const pin = 1111
+
+    const userInputMobileNumber = parseInt(document.getElementById("mobile_input").value)
+    const userInputPin = parseInt(document.getElementById("pin_input").value)
+
+    if(userInputMobileNumber===mobileNumber && pin===userInputPin){
+        window.location.href="./home.html"
+    }
+    else{
+        alert("Invalid Credentials! ")
+    }
+
+
+
 
 })
