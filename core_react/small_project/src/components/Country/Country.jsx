e7ca59a15dc99a16e20946ce436608880215f64f
Salman <rayhanartz@gmail.com>
2026-05-26 18:41:21 +0600

    Flags and Population Extraction

--- core_react/small_project/src/components/Country/Country.jsx
+++ core_react/small_project/src/components/Country/Country.jsx
@@ -2,9 +2,12 @@
 import './Country.css';
 
 export default function Country({country}){
+    console.log(country.population)
     return (
         <div>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt}></img>
             <p>Name: {country.name.common}</p>
+            <p>Population :{country.population.population}</p>
         </div>
     )
 }
